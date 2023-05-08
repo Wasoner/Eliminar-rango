addfedb29ae56e62b8890a72ec3ddf2bffaf5510
Wasoner <cristobal.rivas2001@alumnos.ubiobio.cl>
2023-05-07 21:21:02 -0400

    Codigo funcional

--- Lista.c
+++ Lista.c
@@ -29,24 +29,40 @@ void eliminarRango(Nodo **phead, double y, double z) {
         printf("%d\n", n ->x);
         if ( n -> x >=y && n->x <= z) {
 
+            printf("Cumplio la condicion con: %d\n", n-> x);
+
             f_Rango = 1;
             Nodo *nodoSiguiente = n -> prox ; 
+            //printf("EL nodo sgte es: %d\n", nodoSiguiente ->x);
             if (nodoSiguiente != NULL && nodoAnterior != NULL){
-
+                printf("Primer if\n");
                 //Enlazar el nodo anterior al nodo siguiente
                 nodoAnterior->prox = nodoSiguiente;
+                printf("EL nodo anterior prox es: %d\n",nodoAnterior->x );
                 
             } else if (nodoSiguiente == NULL && nodoAnterior != NULL) {
                 
                 nodoAnterior->prox = NULL;
             }
-
+            //n->prox = NULL;
             free(n);
             n = nodoSiguiente;
             
             
+        } else{
 
-
+            printf("NO Cumplio la condicion con: %d\n", n-> x);
+            if (f_NodoPrincipal == 0){
+                *phead = n;
+                f_NodoPrincipal = 1;
+            }else if (f_Rango == 1)
+            {
+                break;
+            }
+            nodoAnterior = n;
+            printf("EL nodo anterior es: %d\n",nodoAnterior->x );
+            n = n->prox;
+            //printf("EL proximo n es: %d\n", n ->x);
 
         }
     }
@@ -57,15 +73,38 @@ void eliminarRango(Nodo **phead, double y, double z) {
 }
 
 int main(){
+    Nodo* n1 = NULL;
+    n1 = (Nodo*)malloc(sizeof(Nodo));
+    Nodo* n2 = NULL;
+    n2 = (Nodo*)malloc(sizeof(Nodo));
+    Nodo* n3 = NULL;
+    n3 = (Nodo*)malloc(sizeof(Nodo));
+    Nodo* n4 = NULL;
+    n4 = (Nodo*)malloc(sizeof(Nodo));
+    Nodo* n5 = NULL;
+    n5 = (Nodo*)malloc(sizeof(Nodo));
 
-    Nodo n1 = {9, NULL};
-    Nodo n2 = {8, &n1};
-    Nodo n3 = {7, &n2};
-    Nodo n4 = {4, &n3};
-    Nodo n5 = {2, &n4};
+    n1 ->x = 9;
+    n1->prox = NULL;
+    n2 ->x = 8;
+    n2->prox = n1; 
+    n3 ->x = 7;
+    n3->prox = n2;
+    n4 ->x = 4;
+    n4->prox = n3;
+    n5 ->x = 2;
+    n5->prox = n4;    
+  
+  
 
-    Nodo *heap = &n5;
-    eliminarRango(&heap, 4 , 5);
+    Nodo *heap = n5;
+    eliminarRango(&heap, 0 , 1);
+    while (heap != NULL)
+    {
+        printf("%d", heap->x);
+        heap = heap->prox;
+    }
+    
 
     return 0;
 
